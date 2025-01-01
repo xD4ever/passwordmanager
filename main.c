f4b086a682ddc51787d5ea1bfaf45fc3805898eb
xD4ever <boutheljayoussef1@gmail.comgit config --global user.email boutheljayoussef1@gmail.comgit config --global user.email boutheljayoussef1@gmail.com>
2025-01-01 18:48:44 +0100

    Added these functions: to edit a pwd to delete a pwd to view all stored pwd to search for a pwd

--- main.c
+++ main.c
@@ -19,22 +19,74 @@ void add_a_pwd(char *pwdtable, int *number_of_passwords) {
     *number_of_passwords++;
 }
 
-void edit_a_pwd(char *pwdtable) {}
+void edit_a_pwd(char *pwdtable, int *number_of_passwords) {
+    int position = 0;
+    char login[50];
+    char new_password[20];
+    printf("the login for the password:\n");
+    scanf("%s",login);
+    printf("Please enter the new password:\n");
+    scanf("%s",new_password);
+    while (strcmp(*(pwdtable + position), login) != 0 && position < *number_of_passwords) {
+        position++;
+    }
+    *((pwdtable) + position) = new_password;
+}
+
+void delete_a_pwd(char *pwdtable, int *number_of_passwords) {
+    int position = 0;
+    char login[50];
+    printf("enter the login for the information you want to delete:\n");
+    scanf("%s",login);
+    while (strcmp(*(pwdtable + position), login) != 0 && position < *number_of_passwords) {
+        position++;
+    }
+    *((pwdtable) + position) = "";//to empty the cell of the login in the table
+    *((pwdtable + 1) + position) = "";//to empty the cell of the pwd in the
+
+    //to replace the empty space in the table
+    while (position < *number_of_passwords) {
+        *((pwdtable) + position) = *((pwdtable) + (position + 1));
+        *((pwdtable + 1) + position) = *((pwdtable + 1) + (position + 1));
+        position++;
+    }
+    *number_of_passwords--;
+}
+
+void view_all(char *pwdtable, int *number_of_passwords) {
+    for (int i = 0; i < *number_of_passwords; i++) {
+        printf("%s :\n",*((pwdtable) + i));
+        printf("%s :\n",*((pwdtable + 1) + i));
+    }
+}
+
+void search_a_pwd(char *pwdtable, int *number_of_passwords) {
+    int position = 0;
+    char login[50];
+    printf("enter the login for the information you want to delete:\n");
+    scanf("%s",login);
+    while (strcmp(*(pwdtable + position), login) != 0 && position < *number_of_passwords) {
+        position++;
+    }
+    printf("%s :\n",*((pwdtable) + position));
+    printf("%s :\n",*((pwdtable + 1) + position));
+}
 
 int main(void) {
-    char login_and_pwd[10][10];
+    char login_and_pwd[10][10] = {{"youssef","XD4ever"},
+                                    {"hello", "yessir"}};
     int number_of_passwords = strlen(login_and_pwd);
     switch (what_to_do()) {
         //to add a new password
         case 1:add_a_pwd(login_and_pwd, &number_of_passwords);break;
         //to edit a spesific password
-        case 2:edit_a_pwd(login_and_pwd);break;
+        case 2:edit_a_pwd(login_and_pwd, &number_of_passwords);break;
         //to delete the login info for the account
-        //case 3:delete_a_pwd(login_and_pwd);break;
+        case 3:delete_a_pwd(login_and_pwd, &number_of_passwords);break;
         //to see all the stored passwords
-        //case 4:view_all(login_and_pwd);break;
+        case 4:view_all(login_and_pwd, &number_of_passwords);break;
         //to find a specific pwd
-        //case 5:search_a_pwd(login_and_pwd);break;
+        case 5:search_a_pwd(login_and_pwd, &number_of_passwords);break;
         default:printf("Wrong input");break;
     }
     return 0;
